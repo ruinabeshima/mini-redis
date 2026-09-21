6e714e52bb575dd127b3ee83102a1398b34dcc39
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-22 08:07:25 +0900

    feat: log errors instead of fmt

--- app/main.go
+++ app/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net"
 	"os"
+	"log"
 )
 
 func main() {
@@ -14,7 +15,7 @@ func main() {
 	// Redis server runs on PORT 6379
 	ln, err := net.Listen("tcp", ":6379")
 	if err != nil {
-		fmt.Println("Connection failed: ", err)
+		log.Println("Connection failed: ", err)
 		os.Exit(1)
 	}
 	defer ln.Close()
@@ -24,7 +25,7 @@ func main() {
 		// Client connection
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Println("Connection not established")
+			log.Println("Server failed to establish connection: ", err)
 			continue
 		}
 
@@ -50,9 +51,9 @@ func handleConnection(conn net.Conn) {
 		if err != nil {
 			// Handler for standard nc client pressing Ctrl + C (Terminal intercepts lcoally and kills client process immediately)
 			if errors.Is(err, io.EOF) {
-				fmt.Printf("Server closed gracefully: %s\n", remoteAddr)
+				log.Printf("Client connection closed gracefully: %s\n", remoteAddr)
 			} else {
-				fmt.Println("Message not received")
+				log.Print("Message not received: %v\n", err)
 			}
 			return
 		}
@@ -62,7 +63,7 @@ func handleConnection(conn net.Conn) {
 		// Handle Ctrl+C byte for clients running in raw mode (input contains ASCII value 3)
 		if bytes.IndexByte(byteMessage, 0x03) != -1 {
 
-			fmt.Printf("Ctrl+C received from %s. Closing client connection.\n", remoteAddr)
+			log.Printf("Ctrl+C received from %s. Closing client connection.\n", remoteAddr)
 			return
 		}
 
