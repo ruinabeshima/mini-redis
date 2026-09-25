2e3de0b93276750660e0a289d9588166d06ee732
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-25 13:03:40 +0900

    feat: create resp package to parse byte streams

---
+++ resp/helpers.go
@@ -0,0 +1,40 @@
+/*
+	Helper functions to verify CRLF and read lines of data
+*/
+
+package resp
+
+import "errors"
+
+func is_CRLF(byteArray []byte, pointer int) bool {
+
+	// Pointer out of bounds
+	if pointer+1 >= len(byteArray) || pointer < 0 {
+		return false
+	}
+
+	// Check if there is a Carriage Return Line Feed (\r\n)
+	if byteArray[pointer] == '\r' && byteArray[pointer+1] == '\n' {
+		return true
+	}
+
+	return false
+}
+
+/*
+Finds \r\n and returns the slice of bytes from "start" up to (but not including) \r\n
+For simple strings, simple errors, integers
+*/
+func readLine(data []byte, start int) ([]byte, error) {
+	end := start
+	for end < len(data) && !is_CRLF(data, end) {
+		end += 1
+	}
+
+	// \r\n not included
+	if end == len(data) {
+		return nil, errors.New("CRLF not included")
+	}
+
+	return data[start:end], nil
+}
---
+++ resp/types.go
@@ -0,0 +1,52 @@
+package resp
+
+import "errors"
+
+// Data types correspond to symbol of first byte
+const (
+	simpleString = '+'
+	simpleError  = '-'
+	integer      = ':'
+	bulkString   = '$'
+	array        = '*'
+)
+
+type Value struct {
+	Type   byte   // '+', '-', ':', '$', '*'
+	Str    string // Simple string, simple error, bulk string
+	Int    int
+	Array  []Value
+	IsNull bool // Null bulk strings, null array
+}
+
+// Entry function for package: parses incoming byte streams
+func Parse(data []byte) (Value, int, error) {
+	if len(data) == 0 {
+		return Value{}, 0, errors.New("empty payload")
+	}
+
+	switch data[0] {
+	case simpleString:
+		str, consumed, err := parseSimpleString(data)
+		return Value{Type: simpleString, Str: str}, consumed, err
+
+	case simpleError:
+		errStr, consumed, err := parseSimpleError(data)
+		return Value{Type: simpleError, Str: errStr}, consumed, err
+
+	case integer:
+		num, consumed, err := parseInteger(data)
+		return Value{Type: integer, Int: num}, consumed, err
+
+	case bulkString:
+		bstr, isNull, consumed, err := parseBulkString(data)
+		return Value{Type: bulkString, IsNull: isNull, Str: bstr}, consumed, err
+
+	case array:
+		arr, consumed, err := parseArray(data)
+		return arr, consumed, err
+
+	default:
+		return Value{}, 0, errors.New("unknown / invalid command")
+	}
+}
