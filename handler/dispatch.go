780d8714086b07023c4d1af88010c1828129be8f
Rui Nabeshima <ruinabeshima0321@gmail.com>
2026-09-27 00:02:25 +0900

    feat(handler): create dispatch file to execute command with mapping of string to handler function

---
+++ handler/dispatch.go
@@ -0,0 +1,24 @@
+/* 
+	Dispatcher that uses a hashmap for direct lookup 
+	Receives parsed command and arguments from ParseCommand(), and executes said command 
+*/ 
+
+package handler 
+
+import "errors"
+
+// Hashmap with string keys and function values 
+type handlerFunc func(args []string) []byte 
+operations := map[string]handlerFunc{
+	"PING": handlePing
+}
+
+func ExecuteCommand(command Command) []byte {
+	fn, exists = operations[command.Name]
+
+	if !exists {
+		return []byte(fmt.Sprintf("-ERR unknown command '%s'\r\n", cmd.Name))
+	}
+
+	return fn(command)
+}
